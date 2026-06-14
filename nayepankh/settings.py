4d3d0558632cb4cd5a4613f957b93d8f1e1ff30f
RajDevvanshi <anonyomous951989@gmail.com>
2026-06-14 11:59:48 +0530

    Update settings.py

--- nayepankh/settings.py
+++ nayepankh/settings.py
@@ -192,7 +192,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # For production, use Gmail SMTP:
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
