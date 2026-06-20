f8fa6796dcc0cb50bb84c6436d6a0ee5ce29fd69
RajDevvanshi <anonyomous951989@gmail.com>
2026-06-20 16:34:31 +0530

    Update settings.py

--- nayepankh/settings.py
+++ nayepankh/settings.py
@@ -194,7 +194,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # For production, use Gmail SMTP:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
